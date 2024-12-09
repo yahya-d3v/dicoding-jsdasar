da4730822b8133cace94d655324daa10d44964c8
Yahya <yahy4work@gmail.com>
2024-12-09 13:25:26 +0000

    kuis pertama

---
+++ doc/1-mengarungi-lautan-sintaksis.js
@@ -0,0 +1,12 @@
+// TODO: buatlah variabel (konstan) bernama `currency` dan isi dengan nilai "IDR".
+// TODO: buatlah variabel bernama `value` dan isi dengan nilai 10000.
+// TODO: tammbahkan nilai di dalam variabel `value` sebesar 5000.
+/**
+ * TODO: buatlah variabel (konstan) bernama `money`,
+ * isi dengan penambahan string dari nilai `currency` + " " + `value`.
+ */ 
+const currency = "IDR";
+let value = 10000;
+value = value + 5000;
+const money = currency +" "+ value;
+console.log(money);
