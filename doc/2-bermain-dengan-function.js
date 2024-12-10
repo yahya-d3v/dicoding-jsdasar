f6ea38102564d96e02e6752b9de461541b8d967a
Yahya <yahy4work@gmail.com>
2024-12-10 13:58:23 +0000

    kuis kedua

---
+++ doc/2-bermain-dengan-function.js
@@ -0,0 +1,12 @@
+/* TODO:
+ * 1. Buatlah sebuah fungsi bernama `hello` yang menerima 2 argumen: `name` dan `origin`.
+ * 2. Kembalikan fungsi tersebut dengan nilai string dengan templat:
+ *    "Halo! Nama saya (name). Saya tinggal di (origin)."
+ *    Contoh: Jika `name` bernilai 'Fulan' dan `origin` bernilai 'Jakarta', fungsi harus mengembalikan:
+ *    "Halo! Nama saya Fulan. Saya tinggal di Jakarta.".
+ * 3. Argumen `origin` boleh kosong dengan nilai default adalah "Bandung".
+ * 4. Buatlah variabel bernama `rahmat` yang menampung nilai dari hasil pemanggilan fungsi `hello`
+ *    yang diberikan argumen name = 'Rahmat' dan origin = 'Semarang'.
+ * 5. Buatlah variabel bernama `indra` yang menampung nilai dari hasil pemanggilan fungsi `hello`
+ *    yang diberikan argumen name = 'Indra' tanpa memberikan nilai origin.
+ */
