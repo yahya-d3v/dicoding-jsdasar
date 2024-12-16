6722fee32a65e173b512b87be7034807ce67067e
Yahya <yahy4work@gmail.com>
2024-12-16 07:47:29 +0000

    kuis ketiga done

--- doc/3-bukan-sembarang-data.js
+++ doc/3-bukan-sembarang-data.js
@@ -6,10 +6,20 @@ const employees = [
     },
 ];
 
-function addEmployee(name, email, joinYear) {
-    /**
+/**
      * @TODO
      * lengkapi fungsi ini agar dapat menambahkan objek employee baru
      * berdasarkan nilai argumen fungsi dan simpan ke dalam array `employees`
      */
+
+function addEmployee(name, email, joinYear) {
+    /** From here : */
+    const baru = {
+    name : name,
+    email: email,
+    joinYear: joinYear,
+    };
+    employees.push(baru);
 }
+addEmployee('Yahya', 'yahya@gmail.com', '2024');
+console.log(employees);
