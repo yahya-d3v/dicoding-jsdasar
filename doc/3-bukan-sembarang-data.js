e0d9d0e0b64ce1281e9a166be120a1efe6bdf4fc
Yahya <yahy4work@gmail.com>
2024-12-16 07:07:26 +0000

    wip

---
+++ doc/3-bukan-sembarang-data.js
@@ -0,0 +1,15 @@
+const employees = [
+    {
+    name: 'Fulan',
+    email: 'fulan@dicoding.com',
+    joinYear: 2020,
+    },
+];
+
+function addEmployee(name, email, joinYear) {
+    /**
+     * @TODO
+     * lengkapi fungsi ini agar dapat menambahkan objek employee baru
+     * berdasarkan nilai argumen fungsi dan simpan ke dalam array `employees`
+     */
+}
