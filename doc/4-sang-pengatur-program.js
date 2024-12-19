a57b32184e11389fc691daee87f018b71be15b21
Yahya <yahy4work@gmail.com>
2024-12-19 12:28:05 +0000

    wip

---
+++ doc/4-sang-pengatur-program.js
@@ -0,0 +1,26 @@
+function categorizeNumber(input) {
+    /**
+     * TODO:
+     * Lengkapilah fungsi ini agar dapat menghasilkan output berupa kategori bilangan berdasarkan input yang diberikan.
+     * Dengan ketentuan sebagai berikut:
+     * 1. Jika input bukan number, bangkitkan (throw) error.
+     * 2. Jika input adalah bilangan genap, kembalikan dengan nilai "Genap".
+     * 3. Jika input adalah bilangan ganjil, kembalikan dengan nilai "Ganjil".
+     * 4. Jika input adalah bilangan prima, kembalikan dengan nilai "Prima".
+     * 5. Jika input adalah nol, kategorikan kembalikan dengan nilai "Nol".
+     * 6. Jika input negatif, kembalikan dengan nilai "Negatif".
+     */
+}
+
+  // Contoh
+  console.log(categorizeNumber(15)); // Output: "Ganjil"
+  console.log(categorizeNumber(12)); // Output: "Genap"
+  console.log(categorizeNumber(17)); // Output: "Prima"
+  console.log(categorizeNumber(0)); // Output: "Nol"
+  console.log(categorizeNumber(-5)); // Output: "Negatif"
+
+try {
+    categorizeNumber('abc');
+} catch (error) {
+    console.log(error.message);
+} // Output: "Input harus berupa bilangan bulat"
