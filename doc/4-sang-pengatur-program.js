598e25dd2f51e709b2b56511c6f0d3bd3a54ca24
Yahya <yahy4work@gmail.com>
2024-12-23 12:11:00 +0000

    kuis keempat done

--- doc/4-sang-pengatur-program.js
+++ doc/4-sang-pengatur-program.js
@@ -1,4 +1,3 @@
-function categorizeNumber(input) {
     /**
      * TODO:
      * Lengkapilah fungsi ini agar dapat menghasilkan output berupa kategori bilangan berdasarkan input yang diberikan.
@@ -10,49 +9,39 @@ function categorizeNumber(input) {
      * 5. Jika input adalah nol, kategorikan kembalikan dengan nilai "Nol".
      * 6. Jika input negatif, kembalikan dengan nilai "Negatif".
      */
-}
+    function categorizeNumber(input) {
 
-  // Contoh
-  console.log(categorizeNumber(15)); // Output: "Ganjil"
-  console.log(categorizeNumber(12)); // Output: "Genap"
-  console.log(categorizeNumber(17)); // Output: "Prima"
-  console.log(categorizeNumber(0)); // Output: "Nol"
-  console.log(categorizeNumber(-5)); // Output: "Negatif"
+    if(typeof(input)!=='number'){
+    throw new Error(`Input harus berupa bilangan bulat`);
 
-try {
-    categorizeNumber('abc');
-} catch (error) {
-    console.log(error.message);
-} // Output: "Input harus berupa bilangan bulat"
+    } else if(input < 0){
+        return `Negatif`;
+    } else if(input ===0){
+        return `Nol`;
+    } else if(isPrime(input)){
+        return `Prima`;
+    } else if(input %2===0){
+        return `Genap`;
+    } else if (input %2===1) {
+        return `Ganjil`;
+    }};
 
-function checkGrades(grades) {
-    for (let i = 0; i < grades.length; i++) {
-    if (typeof grades[i] !== 'number') {
-        throw new Error('Invalid input. Please provide an array of numbers.');
+    function isPrime(input){ 
+    for(let n = 2; n <= Math.sqrt(input); n++) {
+    if(input %n==0) {
+        return false;
     }
-
-    const grade = grades[i];
-    let predicate;
-
-    if (grade >= 90) {
-        predicate = 'A';
-    } else if (grade >= 80) {
-        predicate = 'B';
-    } else if (grade >= 70) {
-        predicate = 'C';
-    } else if (grade >= 60) {
-        predicate = 'D';
-    } else {
-        predicate = 'E';
     }
-
-    console.log(`Grade ${i + 1}: ${grade} (${predicate})`);
+        return true;
     }
-}
-
-try {
-    const studentGrades = [95, 85, 65, 80, 70];
-    checkGrades(studentGrades);
-} catch (e) {
-    console.error(e);
-}
+      // Contoh
+      console.log(categorizeNumber(15)); // Output: "Ganjil"
+      console.log(categorizeNumber(12)); // Output: "Genap"
+      console.log(categorizeNumber(17)); // Output: "Prima"
+      console.log(categorizeNumber(0)); // Output: "Nol"
+      console.log(categorizeNumber(-5)); // Output: "Negatif"
+    try {
+        categorizeNumber('abc');
+    } catch (error) {
+        console.log(error.message);
+      } // Output: "Input harus berupa bilangan bulat"
